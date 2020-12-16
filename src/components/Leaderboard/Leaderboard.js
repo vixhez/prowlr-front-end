6d9843583f6139ff5a97ebf92cefe7eec05ac15b
tom <tomhall4321@gmail.com>
2020-12-16 15:33:32 +0000

    podium component created

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,16 +1,4 @@
-// const Leaderboard = ({ getLeaderBoard }) => (
-
-//     <div>
-//         <ol className="list-group">
-//             <li className="list-group-item">Cras justo odio</li>
-//             <li className="list-group-item">Dapibus ac facilisis in</li>
-//             <li className="list-group-item">Morbi leo risus</li>
-//             <li className="list-group-item">Porta ac consectetur ac</li>
-//             <li className="list-group-item">Vestibulum at eros</li>
-//         </ol>
-//     </div>
-// );
-
+import Podium from './Podium';
 import { Component } from 'react';
 
 class Leaderboard extends Component {
@@ -40,28 +28,30 @@ class Leaderboard extends Component {
 
         return (
             // Insert subcomponent Podium - pass in top3 variable. 
-
-            <table className="table mt-5">
-                <thead>
-                    <tr>
-                        <th scope="col">Position</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Image</th>
-                        <th scope="col">Liked</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {runnersUp.map((animal, index) => (
-                        <tr key={index}>
-                            <th scope="row">{index + 4}</th>
-                            <td>{animal.name}</td>
-                            <td>{animal.image_url}</td>
-                            <td>{animal.percentageOfLikes}</td>
+            <>
+                <Podium top3={top3} />
+                <table className="table mt-5">
+                    <thead>
+                        <tr>
+                            <th scope="col">Position</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Liked</th>
                         </tr>
-                    )
-                    )}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {runnersUp.map((animal, index) => (
+                            <tr key={index}>
+                                <th scope="row">{index + 4}</th>
+                                <td>{animal.name}</td>
+                                <td>{animal.image_url}</td>
+                                <td>{animal.percentageOfLikes}</td>
+                            </tr>
+                        )
+                        )}
+                    </tbody>
+                </table>
+            </>
         )
     }
 }
