ac73433cf698cd52ccec4443434788c8510fd125
tom <tomhall4321@gmail.com>
2020-12-17 13:16:28 +0000

    changed axios-config

--- src/data/axios-config.js
+++ src/data/axios-config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // create a version of axios with useful defaults
 export default axios.create({
-    baseURL: "http://ec2-3-9-13-137.eu-west-2.compute.amazonaws.com/api/",
+    baseURL: "https://prowlr.developme.space/api/",
     headers: {
         // make sure we get JSON back
         Accept: "application/json",
